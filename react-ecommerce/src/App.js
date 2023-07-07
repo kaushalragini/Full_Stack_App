4b67b0728c9768a1d8e1047b8b250560d5c70a34
Ragini Kaushal <ragini93.11@gmail.com>
2023-07-07 19:59:35 +0530

    tailwind

--- react-ecommerce/src/App.js
+++ react-ecommerce/src/App.js
@@ -1,12 +1,10 @@
 import React from "react";
-
-import { Counter } from "./features/counter/Counter";
-import "./App.css";
+import ProductList from "./features/counter/product-list/ProductList";
 
 function App() {
   return (
     <div className="App">
-      <Counter></Counter>
+      <ProductList />
     </div>
   );
 }
